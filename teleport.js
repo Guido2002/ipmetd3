0b32dc97b9e55389703440811b3e02de990ad389
Guido2002 <guidoduif2002@gmail.com>
2022-01-04 17:47:01 +0100

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -3,7 +3,7 @@ window.onload = () =>{
     const camera = document.getElementById('js--camera');
   
     for (let i = 0; i < places.length; i++) {
-      places[i].addEventListener('click', function(evt){
+      places[i].addEventListener('click', function(){
         let att = document.createAttribute("animation");
         att.value = "property: position; easing: linear; dur: 2000; to: " + (this.getAttribute('position').x + 0.5)+ " 1.6 " + this.getAttribute('position').z;
         camera.setAttribute('animation', att.value);
