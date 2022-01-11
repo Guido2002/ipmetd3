f740908737bc503d7f9601d8fc59f1d93b28a9dd
Guido2002 <guidoduif2002@gmail.com>
2022-01-11 10:38:04 +0100

    distanceCalculator toegevoegd

--- js/main.js
+++ js/main.js
@@ -24,14 +24,16 @@ function setDrugEffect() {
      const drug_physiqal = document.getElementsByClassName("drug-object")
      const vid1 = document.getElementById("video1");
      const vid2 = document.getElementById("video2");
+
+     drug1.setAttribute("sound", "src: url(/audio/drug_video1.wav); autoplay: true; loop: true;")
+     drug2.setAttribute("sound", "src: url(/audio/drug_video2.wav); autoplay: true; loop: true;")
      
-    for (let j = 0; j < drug_physiqal.length; j++) {
-        const drug_location = drug_physiqal[j];
-        vid1.currentTime = 0;
-        vid2.currentTime = 0;
-        drug1.setAttribute("sound", "src: url(/audio/drug_video1.wav); autoplay: true; loop: true;")
-        drug2.setAttribute("sound", "src: url(/audio/drug_video2.wav); autoplay: true; loop: true;")
-        drug_location.setAttribute("position", "100 100 100");
+    vid1.currentTime = 0;
+    vid2.currentTime = 0;
+
+    for (let i = 0; i < drug_physiqal.length; i++) {
+        const druglocation = drug_physiqal[i];
+        druglocation.setAttribute("position", "100 100 100");
     }
 }
 
